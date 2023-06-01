ecd42d009ff8b6fbe23a77dc46bdd4f87b4f1fe8
Tim <neshtimo38@gmail.com>
2023-06-01 21:24:58 +0300

    will refactor account screen

--- carpool/src/Components/SearchRides.js
+++ carpool/src/Components/SearchRides.js
@@ -1,15 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 import { Text, Button, Input } from "@rneui/base";
 import { View, StyleSheet } from "react-native";
-
+import { AntDesign } from "@expo/vector-icons";
+import { Ionicons } from "@expo/vector-icons";
 const SearchRides = () => {
+  const [searchTerm, setSearchTerm] = useState();
   return (
-    <View>
+    <View style={styles.container}>
+      {/* <Ionicons name="person" size={24} color="black" /> */}
       {/* icon goes here */}
-      <Input />
+      <Input
+        placeholder="Where to?"
+        value={searchTerm}
+        onChangeText={(newItem) => setSearchTerm(newItem)}
+        rightIcon={<AntDesign name="search1" size={24} color="#6d7c92" />}
+      />
       {/* search bar */}
     </View>
   );
 };
+const styles = StyleSheet.create({
+  container: {
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+  },
+});
 
 export default SearchRides;
